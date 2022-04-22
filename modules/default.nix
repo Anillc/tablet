1972af6adbb2b20411013b398042028a9e1cede2
Anillc <i@anillc.cn>
2022-04-22 18:50:49 +0800

    add binfmt

--- modules/default.nix
+++ modules/default.nix
@@ -20,4 +20,15 @@ with lib;
     enable = true;
     package = pkgs.mariadb;
   };
+  nix.extraOptions = ''
+    extra-platforms = aarch64-linux i686-linux
+  '';
+  nix.sandboxPaths = [ "/run/binfmt" "${pkgs.qemu}" ];
+  boot.binfmt.registrations = {
+    aarch64 = {
+      interpreter = "${pkgs.qemu}/bin/qemu-aarch64";
+      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'';
+      mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff'';
+    };
+  };
 }
