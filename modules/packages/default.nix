9deba20fd38898bd2af30cfaaf1ad857dea512b7
Anillc <i@anillc.cn>
2022-08-06 16:22:12 +0800

    add share-text

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -35,8 +35,8 @@ in {
     pkgs.inputs.nixos-cn.legacyPackages.${pkgs.system}.netease-cloud-music
     jetbrains.idea-community jetbrains.goland jetbrains.clion go_1_18 gcc
     nix-index clang cmake gnumake mtr android-studio
-    xorg.xbacklight xorg.xmodmap scrcpy share libnotify
-  ];
+    xorg.xbacklight xorg.xmodmap scrcpy libnotify
+  ] ++ share;
   nixpkgs.overlays = [(self: super: {
     picom = super.picom.overrideAttrs (x: {
       src = pkgs.fetchFromGitHub {
--- modules/packages/share.nix
+++ modules/packages/share.nix
@@ -1,28 +1,41 @@
-{ kdeconnect, xclip, lib, libnotify, writeScriptBin, ... }:
+{ runtimeShell, kdeconnect, xclip, lib, libnotify, writeScriptBin, ... }:
 
 with builtins;
 with lib;
 
-writeScriptBin "share-file" ''
-  export PATH=$PATH:${makeBinPath [ xclip kdeconnect libnotify ]}
-  ID=$(kdeconnect-cli -a --id-only)
-  if [ -z "$ID" ]; then
-    echo "Coudn't find a device."
-    notify-send 'share-file' "Coudn't find a device."
-    exit 1
-  fi
-  if [ ! -z "$1" ]; then
-    kdeconnect-cli -d $ID --share $1
+let
+  same = ''
+    #!${runtimeShell}
+    set -e
+    export PATH=$PATH:${makeBinPath [ xclip kdeconnect libnotify ]}
+    ID=$(kdeconnect-cli -a --id-only)
+    if [ -z "$ID" ]; then
+      echo "Coudn't find a device."
+      notify-send 'share-file' "Coudn't find a device."
+      exit 1
+    fi
+  '';
+in [
+  (writeScriptBin "share-file" ''
+    ${same}
+    if [ ! -z "$1" ]; then
+      kdeconnect-cli -d $ID --share $1
+      notify-send 'share-file' '✓'
+      exit 0
+    fi
+    IMG=$(mktemp).png
+    xclip -selection clipboard -t image/png -o > $IMG
+    if [ ! $? -eq 0 ]; then
+      echo "No image in clipboard"
+      notify-send 'share-file' "No image in clipboard"
+      exit 1
+    fi
+    kdeconnect-cli -d $ID --share $IMG
     notify-send 'share-file' '✓'
-    exit 0
-  fi
-  IMG=$(mktemp).png
-  xclip -selection clipboard -t image/png -o > $IMG
-  if [ ! $? -eq 0 ]; then
-    echo "No image in clipboard"
-    notify-send 'share-file' "No image in clipboard"
-    exit 1
-  fi
-  kdeconnect-cli -d $ID --share $IMG
-  notify-send 'share-file' '✓'
-''
+  '')
+  (writeScriptBin "share-text" ''
+    ${same}
+    kdeconnect-cli -d $ID --share-text "$*"
+    notify-send 'share-file' '✓'
+  '')
+]
