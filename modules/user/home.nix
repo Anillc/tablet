135095384c2243f7def048cf73da0e100146586b
Anillc <i@anillc.cn>
2023-02-17 19:15:03 +0800

    add fish plugins

--- modules/user/home.nix
+++ modules/user/home.nix
@@ -27,6 +27,24 @@ with lib;
           sha256 = "sha256-MfrRQdcj7UtIUgtqKjt4lqFLpA6YZgKjE03VaaypNzE=";
         };
       }
+      {
+        name = "done";
+        src = pkgs.fetchFromGitHub {
+          owner = "franciscolourenco";
+          repo = "done";
+          rev = "d6abb267bb3fb7e987a9352bc43dcdb67bac9f06";
+          sha256 = "sha256-6oeyN9ngXWvps1c5QAUjlyPDQwRWAoxBiVTNmZ4sG8E=";
+        };
+      }
+      {
+        name = "puffer";
+        src = pkgs.fetchFromGitHub {
+          owner = "nickeb96";
+          repo = "puffer-fish";
+          rev = "fd0a9c95da59512beffddb3df95e64221f894631";
+          sha256 = "sha256-aij48yQHeAKCoAD43rGhqW8X/qmEGGkg8B4jSeqjVU0=";
+        };
+      }
     ];
   };
   programs.starship = {
