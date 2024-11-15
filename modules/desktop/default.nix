cba22f4f14b728d3a5467be2c276066c110231c4
Anillc <i@anillc.cn>
2024-11-15 21:01:46 +0800

    add fcitx plugins

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -24,7 +24,12 @@ with lib;
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-chinese-addons ];
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-pinyin-zhwiki
+      fcitx5-pinyin-moegirl
+      fcitx5-pinyin-minecraft
+    ];
   };
   fonts.packages = with pkgs; [
     jetbrains-mono
